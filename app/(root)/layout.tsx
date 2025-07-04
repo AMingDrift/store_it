82805a65e81048f5c9c8b0f3d11d62b0e264ec98
WMC <18013550018@163.com>
2025-07-04 16:06:49 +0800

    feat: mobile navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,13 +7,14 @@ import React from "react";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
+  console.log(currentUser);
 
   if (!currentUser) return redirect("/sign-in");
   return (
     <main className="flex h-screen">
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation />
+        <MobileNavigation {...currentUser} />
         <Header />
 
         <div className="main-content">{children}</div>
